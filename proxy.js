phuquoc164/AAAforREST

commit 44534427411bf54e360720f12af9e4a109db22c9

FIX: Simplified configuration by using defaults.
Fallback rule: the proxy is always on.

proxy.js
@@ -241,9 +241,7 @@ http.createServer(function(requestIn, responseOut) {
       }
       i++;
     }
-    if (!found) {
-      proxyWork(context);
-    }
+    proxyWork(context); //Fallback rule
   }
   });
 }).listen(configuration.port);

config.sample.js
@@ -82,10 +82,16 @@ module.exports = {
       "ammo": ["elmer.fudd"]
     },
 
+    /**
+     * Rules defining the `action` to be taken
+     * when `control` is true.
+     * Note: A fallback rule is always on. It is defined as:
+     * `control: "true"` and `action: "proxyWork(context)"`.
+     */
     rules: [{
       control: function(context) {
-	      return context.requestIn.method != 'GET';
-	    },
+        return context.requestIn.method != 'GET';
+      },
       action: function(context) {
         authenticate(context, function() {
           AuthorizList(context, function() {
@@ -93,10 +99,6 @@ module.exports = {
           });
         })
       },
-      final: true
-    }, {
-      control: "true",
-      action: "proxyWork(context);",
       final: true
     }]
   }]

commit 973e5a83592774c05c055425f278b72f1a1b7594

FIX: Exception when a rule is applied.
Regression caused by 44534427411bf54e360720f12af9e4a109db22c9.

proxy.js
@@ -221,7 +221,7 @@ http.createServer(function(requestIn, responseOut) {
       }
       i++;
     }
-    proxyWork(context); //Fallback rule
+    if (!found) proxyWork(context); //Fallback rule
   }
   });
 }).listen(configuration.port);