8284091987563b2b03d6565f8036b8d021cc696f
Aurelien Benel <aurelien.benel@utt.fr>
2015-04-28 12:19:18 +0200

    TEST: Public, community and private authorization schemes.
    Refactored also the authentication test in order to use the same sample project.

--- spec/api/authentication_spec.js
+++ spec/api/authentication_spec.js
@@ -1,12 +1,12 @@
 var test = require('frisby');
 
-test.create('Dummy authentication OK')
-  .get('http://traduxio.local:1337/works/')
+test.create('Dummy authentication with a valid user')
+  .get('http://cassandre.local:1337/text/Wonderland/')
   .auth('alice', 'whiterabbit')
   .expectStatus(200)
   .toss();
-test.create('Dummy authentication KO')
-  .get('http://traduxio.local:1337/works/')
+test.create('Dummy authentication with an invalid user')
+  .get('http://cassandre.local:1337/text/Wonderland/')
   .auth('alice', 'rabbit')
   .expectStatus(401)
   .toss();
---
+++ spec/api/authorization_spec.js
@@ -0,0 +1,39 @@
+var test = require('frisby');
+
+test.create('Public authorization with anonymous read')
+  .get('http://cassandre.local:1337/text/')
+  .expectStatus(200)
+  .toss();
+test.create('Public authorization with valid user read')
+  .get('http://cassandre.local:1337/text/')
+  .auth('alice', 'whiterabbit')
+  .expectStatus(200)
+  .toss();
+test.create('Public authorization with anonymous write')
+  .post('http://cassandre.local:1337/', {}, {json:true})
+  .expectStatus(401)
+  .toss();
+test.create('Public authorization with valid user write')
+  .post('http://cassandre.local:1337/', {}, {json:true})
+  .auth('alice', 'whiterabbit')
+  .expectStatus(201)
+  .toss();
+test.create('Community authorization with anonymous')
+  .get('http://cassandre.local:1337/text/Wonderland/')
+  .expectStatus(401)
+  .toss();
+test.create('Community authorization with valid user')
+  .get('http://cassandre.local:1337/text/Wonderland/')
+  .auth('alice', 'whiterabbit')
+  .expectStatus(200)
+  .toss();
+test.create('Private authorization with other user')
+  .get('http://cassandre.local:1337/text/Wonderland/iamlate')
+  .auth('alice', 'whiterabbit')
+  .expectStatus(401)
+  .toss();
+test.create('Private authorization with owner')
+  .get('http://cassandre.local:1337/text/Wonderland/iamlate')
+  .auth('mrwhite', 'alice')
+  .expectStatus(200)
+  .toss();
