a7f429d6f78e97d1e8cbe7e5d11f5f44f0c34951
Aurelien Benel <aurelien.benel@utt.fr>
2015-05-22 18:12:46 +0200

    TEST: Authenticators priority.
    If the login has been found by an authenticator 
    and the password check failed, it shouldn't be 
    checked by other authenticators.

--- spec/api/backend_authentication_spec.js
+++ spec/api/backend_authentication_spec.js
@@ -1,22 +1,47 @@
 var test = require('frisby');
 
-test.create('Dummy authentication with a valid user')
-  .get('http://cassandre.local:1337/text/Wonderland/')
+test.create('Fixed authentication succeeded')
+  .get('http://auth_fixed.local:1337/')
   .auth('alice', 'whiterabbit')
   .expectStatus(200)
   .toss();
-test.create('Dummy authentication with an invalid user')
-  .get('http://cassandre.local:1337/text/Wonderland/')
+test.create('Fixed authentication failed on password: no fallback')
+  .get('http://auth_fixed.local:1337/')
   .auth('alice', 'rabbit')
   .expectStatus(401)
   .toss();
-test.create('HTTP authentication with a valid user')
-  .get('http://cassandre.local:1337/text/Wonderland/')
+test.create('Fixed authentication failed on login: fallback')
+  .get('http://auth_fixed.local:1337/')
+  .auth('mrwhite', 'alice')
+  .expectStatus(200)
+  .toss();
+
+test.create('HTTP authentication succeeded')
+  .get('http://auth_http.local:1337/')
   .auth('hatter', 'teaparty')
   .expectStatus(200)
   .toss();
-test.create('HTTP authentication with an invalid user')
-  .get('http://cassandre.local:1337/text/Wonderland/')
-  .auth('hatter', 'tea')
+// SECURITY WARNING: HTTP authentication does not provide ways to know which
+// part of the credentials failed. Whatever its position in the authenticators
+// list, it is always considered at the lowest priority.
+test.create('HTTP authentication failed on password: fallback')
+  .get('http://auth_http.local:1337/')
+  .auth('mrwhite', 'alice')
+  .expectStatus(200)
+  .toss();
+
+test.create('LDAP authentication succeeded')
+  .get('http://auth_ldap.local:1337/')
+  .auth('riemann', 'password')
+  .expectStatus(200)
+  .toss();
+test.create('LDAP authentication failed on password: no fallback')
+  .get('http://auth_ldap.local:1337/')
+  .auth('riemann', 'secret')
   .expectStatus(401)
+  .toss();
+test.create('LDAP authentication failed on password: no fallback')
+  .get('http://auth_ldap.local:1337/')
+  .auth('mrwhite', 'alice')
+  .expectStatus(200)
   .toss();
