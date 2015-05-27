phuquoc164/AAAforREST

commit 5201f282d256705d6c75e895014cb93cf480e216

SECURITY FIX: Authenticators priority.
If the login has been found by an authenticator
and the password check failed, it shouldn't be
checked by other authenticators.

Major refactoring of:

- general authentication mechanism,
- LDAP authentication implementation.

app/authenticator.ldap.js
@@ -2,87 +2,64 @@ module.exports = function() {
 
   var LDAP = require('ldapjs');
   var CRYPTO = require ('crypto');
-  var URL = require('url');
 
-  var cache = {};
-
-  var flush = function(id, server) {
-    console.log('flushing ldap auth ' + id + ' from server ' + server);
-    delete cache[server][id];
-    if (cache[server] === {}) delete cache[server];
+  var cache = {
+    set: function(url, id, err) {
+      console.log('CACHE set');
+      if (!this[url]) {
+        this[url] = {};
+      }
+      this[url][id] = {
+        success: !err,
+        timeOut: setTimeout(this.remove, err? 300000:900000, url, id)
+      };
+    },
+    get: function(url, id) {
+      console.log('CACHE got');
+      return this[url, id];
+    },
+    remove: function(url, id) {
+      console.log('CACHE removed');
+      delete cache[url][id];
+      if (cache[url]==={}) {
+        delete cache[url];
+      }
+    }
   };
 
   return function(context, settings, callback) {
-    if (!context.login) return callback(false);
-    var negativeCacheTime = (settings.negativeCacheTime || 300) /* 5 minutes*/ * 1000 /*ms*/;
-    var positiveCacheTime = (settings.positiveCacheTime || 900) /* 15 minutes*/ * 1000 /*ms*/;
     var url = settings.url;
-    var ldapReq = settings.id + '=' + context.login + ',' + settings.dn; //do not manage more than one dc information
+    var ldapReq = settings.id + '=' + context.auth.login + ',' + settings.dn;
     var id = CRYPTO.createHash('sha1')
-      .update(url).update(ldapReq).update(context.pw||"")
+      .update(url)
+      .update(ldapReq)
+      .update(context.auth.password || "")
       .digest('hex');
-    var login = context.login;
-    if (typeof settings.domain != 'undefined') {
-      var domain = settings.domain;
-      if (domain && typeof domain == 'string') {
-        domain = settings.domain;
-      } else {
-        domain = URL.parse(url).host;
-      }
-      if (domain)
-        login = context.login + '@' + domain;
-    }
-
-    var maxWaits=20;
-    var waits=0;
-
-    checkAuth();
-
-    function checkAuth() {
-      if (!cache[url] || !cache[url][id]) {
-        console.log('Logging in ' + ldapReq + ' into ' + url);
-        if (!cache[url]) {
-          cache[url] = {};
-        }
-        if (!(id in cache[url])) {
-          cache[url][id] = {};
-        }
-        var ldap = LDAP.createClient({
-          'url': url
-        });
-        ldap.bind(ldapReq, context.pw, function(err) {
-          if ('timeOut' in cache[url][id]) {
-            clearTimeout(cache[url][id].timeOut);
-          }
-          cache[url][id].err = err;
-          cache[url][id].timeOut =
-            setTimeout(flush, err?negativeCacheTime:positiveCacheTime, id, url);
-          var isAuthentified = !err;
-          if (isAuthentified) {
-            context.login = login;
-            ldap.unbind(function () {
-              callback(isAuthentified);
-            });
-          } else {
-            console.log('LDAP error: ' + JSON.stringify(err));
-            callback(isAuthentified);
-          }
-        });
-      } else {
-        if (cache[url][id].hasOwnProperty("err")) {
-          if (!cache[url][id].err) context.login = login;
-          callback(!cache[url][id].err);
+    var cachedValue = cache.get(url, id);
+    if (cachedValue) {
+      context.auth.success = cachedValue.success;
+      callback(true);
+    } else {
+      var ldap = LDAP.createClient({url: url});
+      ldap.search(ldapReq, {}, function(err, res) {
+        if (err) { // network error
+          callback(false);
         } else {
-           waits++;
-           if (waits>maxWaits) {
-             delete cache[url][id];
-             waits=0;
-             checkAuth();
-           } else {
-             setTimeout(checkAuth,100);
-           }
+          res.on('error', function(err) { // ldap error including 'not found'
+            callback(false);
+          });
+          res.on('searchEntry', function() {
+            ldap.bind(ldapReq, context.auth.password, function(err) {
+              context.auth.success = !err;
+              cache.set(url, id, err);
+              if (!err) {
+                ldap.unbind();
+              }
+              callback(true);
+            });
+          });
         }
-      }
+      });
     }
   };
 

app/accounter.log.js
@@ -21,7 +21,7 @@ module.exports = function() {
     var site = configuration.sites[context.conf];
     var logger = loggers.get(site?site.hostProxy:'misc');
     logger.info(
-      (context.login? context.login + '@' : '')
+      (context.auth? context.auth.login + '@' : '')
       + (request.headers['x-forwarded-for'] || request.connection.remoteAddress)
       + '\t' + request.method 
       + '\t' + request.headers.host + request.url 

app/authenticator.cookie.js
@@ -20,9 +20,11 @@ function parseBody(context,sessionHandler) {
   var post=require("querystring").parse(context.options.body);
   var userfield=sessionHandler.userfield || "name";
   var passfield=sessionHandler.passfield || "password";
-  context.login=post[userfield] || "";
-  context.pw=post[passfield] || "";
-};
+  context.auth = {
+    login: post[userfield] || "",
+    password: post[passfield] || ""
+  };
+}
 
 function handleSessionRequest(sessionHandler) {
   var $=this;
@@ -56,7 +58,7 @@ function handleSessionRequest(sessionHandler) {
       });
       break;
     case "DELETE":
-      delete $.context.login;
+      delete $.context.auth;
       setAuthCookie($.context,sessionHandler);
       if (sessionHandler.forward) {
         $.proxyWork($.context);
@@ -69,10 +71,10 @@ function handleSessionRequest(sessionHandler) {
         if (sessionHandler.forward) {
           $.proxyWork($.context);
         } else {
-          var session={
-            name:$.context.login || null,
-            authenticator:authenticator
-          }
+          var session = {
+            name: $.context.auth.login || null,
+            authenticator: authenticator
+          };
           $.sendResponse($.context, 200, session);
         }
       });
@@ -134,8 +136,8 @@ function ignoreCookie(context,authenticator) {
 function checkAuthCookie(context,authenticator,callback) {
   var currcookie;
   authenticator.cookieName=authenticator.cookieName || default_cookie_auth_name;
-  if (context.login || context.cookie && context.cookie.ignore
-    && context.cookie.ignore.indexOf(authenticator.cookieName) !=-1) {
+  if (context.auth || context.cookie && context.cookie.ignore
+    && context.cookie.ignore.indexOf(authenticator.cookieName)!==-1) {
     return callback(false);
   }
   var expiryTime=authenticator.sessionLength || defaultExpiryTime;
@@ -145,8 +147,11 @@ function checkAuthCookie(context,authenticator,callback) {
     if (auth_info=saved_cookies[currcookie]) {
       if (auth_info.timestamp<new Date().getTime()+expiryTime && auth_info.login) {
         auth_info.timestamp=new Date();
-        context.login=auth_info.login;
-        console.log("authenticating "+context.login+" through cookie "+currcookie);
+        context.auth = {
+          login: auth_info.login,
+          success: true
+        };
+        console.log("authenticating " + context.auth.login + " through cookie " + currcookie);
         return callback(true);
       } else {
         console.log("expired");
@@ -162,20 +167,21 @@ function checkAuthCookie(context,authenticator,callback) {
 
 function setAuthCookie(context,authenticator) {
   var currcookie;
+  var auth_info;
   authenticator.cookieName=authenticator.cookieName || default_cookie_auth_name;
   var expiryTime=authenticator.sessionLength || defaultExpiryTime;
   var cookies=new Cookies(context.requestIn,context.responseOut);
   if (currcookie=cookies.get(authenticator.cookieName)) {
     if (auth_info=saved_cookies[currcookie]) {
-      if (context.login && auth_info.login!=context.login) {
+      if (context.auth && auth_info.login!==context.auth.login) {
         console.log("switching authentication from "+auth_info.login
-            +" to "+context.login);
+            + " to " + context.auth.login);
       }
     } else {
       auth_info={};
     }
     auth_info.timestamp=new Date().getTime();
-  } else if (context.login) {
+  } else if (context.auth) {
     var ok=false;
     while (!ok) {
       currcookie=crypto.createHash('sha1').update(authenticator.cookieName).update("plipplop"+new Date().getTime()).digest('hex');
@@ -185,9 +191,9 @@ function setAuthCookie(context,authenticator) {
     console.log("new session "+currcookie);
     auth_info={timestamp:new Date().getTime()}
   }
-  if (context.login) {
-    console.log("authenticating "+context.login);
-    auth_info.login=context.login;
+  if (context.auth) {
+    console.log("authenticating " + context.auth.login);
+    auth_info.login = context.auth.login;
     saved_cookies[currcookie]=auth_info;
   } else {
     if (currcookie) {

app/authenticator.http.js
@@ -9,10 +9,13 @@ module.exports = function() {
       hostname: service.hostname,
       port: service.port || 80,
       path: service.path,
-      auth: context.login + ':' + context.pw
+      auth: context.auth.login + ':' + context.auth.password
     }, function(response) {
-      callback(response.statusCode=='200');
+      if (response.statusCode=='200') {
+        context.auth.success = true;
+      }
+      callback(context.auth.success);
     });
-  }
+  };
 
 }();

app/proxy.js
@@ -9,6 +9,7 @@ var ldap = require('./authenticator.ldap');
 var basic = require('./authenticator.http');
 var cookie = require('./authenticator.cookie');
 var log = require('./accounter.log');
+var forwardAuth = require('couch-proxy-auth');
 
 configuration.sites.forEach(cookie.manageSession);
 
@@ -37,41 +38,51 @@ function tryAgain(context) {
 /*
  * Default implementation of checkCredentials (with fixed login and password)
  * that can be replaced for various authentication formats and protocols.
+ * Return true if the login was found.
+ * Set `context.auth.success` to true if the credentials were good.
  */
 var dummy = function(context, settings, callback) {
-  callback(context.login && context.login==settings.login && context.pw==settings.password);
+  if (context.auth.login==settings.login) {
+    context.auth.success = context.auth.password==settings.password;
+  }
+  callback(context.auth.success!==undefined);
 };
 
 function authenticateIfPresent(context, callback) {
-  var authenticationPresent=context.login ? true : false;
-  authenticate(context,callback,!authenticationPresent);
-};
+  authenticate(context, callback, context.auth===undefined);
+}
 
 function authenticate(context, callback, shouldNotCatch) {
     var authenticators = configuration.sites[context.conf].authentication;
     async.detectSeries(authenticators, function(authenticator, callback) {
-      if (authenticator.url) {
+      if (context.auth) {
         if (authenticator.dn) {
           ldap(context, authenticator, callback);
-        } else {
+        } else if (authenticator.url) {
           basic(context, authenticator, callback);
+        } else {
+          dummy(context, authenticator, callback);
         }
-      } else if (authenticator.hasOwnProperty("cookieName")) {
+      } else if (context.requestIn.headers.cookie) {
         cookie.check(context, authenticator, callback);
       } else {
-        dummy(context, authenticator, callback);
+        callback();
       }
-    }, function(successfulAuthenticator) {
-      if (successfulAuthenticator) {
-        if (!successfulAuthenticator.preserveCredentials) {
+    }, function(authenticator) {
+      if (context.auth && context.auth.success) {
+        if (!authenticator.preserveCredentials) {
           delete context.options.headers.Authorization;
         }
         var site=configuration.sites[context.conf];
-        if (site.forwardedLoginHeader && context.login) {
-          context.options.headers[site.forwardedLoginHeader] = context.login;
+        if (site.forwardedLoginHeader) {
+          context.options.headers[site.forwardedLoginHeader] = context.auth.login;
         }
-        if (site.forwardedLoginSecret && context.login) {
-          var addedHeaders=require('couch-proxy-auth')(context.login,site.forwardedLoginRoles || 'protect',site.forwardedLoginSecret);
+        if (site.forwardedLoginSecret) {
+          var addedHeaders = forwardAuth(
+            context.auth.login,
+            site.forwardedLoginRoles || 'protect',
+            site.forwardedLoginSecret
+          );
           for (var newHeader in addedHeaders) {
             // since roles are forced to a value, the role header is given by couch-proxy-auth
             // so can't be forged
@@ -79,9 +90,8 @@ function authenticate(context, callback, shouldNotCatch) {
             context.options.headers[newHeader] = addedHeaders[newHeader];
           }
         }
-        callback(successfulAuthenticator);
+        callback(true);
       } else {
-        delete context.login;
         //if authorization header is present, it means that preserveCredentials
         //is true, so we authorize to forward the request to upstream server
         if (context.options.headers.Authorization || shouldNotCatch) {
@@ -106,7 +116,7 @@ function authorize(context, callback) {
   var resourceMatch, userMatch;
   for (var uriPart in acl) {
     resourceMatch = context.requestIn.url.indexOf(uriPart) > -1;
-    userMatch =  acl[uriPart].indexOf(context.login) > -1;
+    userMatch =  acl[uriPart].indexOf(context.auth.login) > -1;
     if (resourceMatch) break;
   }
   if (resourceMatch && !userMatch) {
@@ -244,8 +254,10 @@ function parseHttpCredentials(context) {
     var token = authorization.split(" ");
     if (token[0]=='Basic' && token.length>1) {
       var credentials = new Buffer(token[1], 'base64').toString().split(':');
-      context.login = credentials[0];
-      context.pw = credentials.length>1 ? credentials[1] : "";
+      context.auth = {
+        login: credentials[0],
+        password: credentials.length>1 ? credentials[1] : ""
+      };
     }
   }
 }

commit 2a930ecda44edad39a20831906c955243372e270

FIX: Remove from LDAP cache (broken by 5201f28).

app/authenticator.ldap.js
@@ -19,10 +19,12 @@ module.exports = function() {
       return this[url]? this[url][id] : null;
     },
     remove: function(url, id) {
-      console.log('CACHE removed');
-      delete this[url][id];
-      if (this[url]==={}) {
-        delete this[url];
+      if (this[url]) {
+        console.log('CACHE removed');
+        delete this[url][id];
+        if (this[url]==={}) {
+          delete this[url];
+        }
       }
     }
   };