646ca3ea05f2334f1b9b8b49b144247d3c9d08b0
Aurelien Benel <aurelien.benel@utt.fr>
2017-11-04 11:55:34 +0100

    TEST: Get site cookie information.
    `GET /_session` should return a JSON payload
    with `name` being equal to the user’s login
    if cookie is set, or null instead.

--- spec/api/frontend_authentication_spec.js
+++ spec/api/frontend_authentication_spec.js
@@ -5,14 +5,31 @@ test.create('HTTP basic authentication')
   .auth('alice', 'whiterabbit')
   .expectStatus(200)
   .toss();
+
 test.create('Site cookie authentication')
-  .post('http://cassandre.local:1337/_session', {name:'alice', password:'whiterabbit'})
+  .get('http://cassandre.local:1337/_session')
   .expectStatus(200)
-  .after(function(error, resource) {
-    test.create('HTTP cookie authentication use')
-      .get('http://cassandre.local:1337/text/Wonderland/')
-      .addHeader('Cookie', resource.headers['set-cookie'])
+  .expectJSON({name: null})
+  .after(function() {
+    test.create('Cookie creation')
+      .post('http://cassandre.local:1337/_session', {name:'alice', password:'whiterabbit'})
       .expectStatus(200)
+      .after(function(error, resource) {
+        var cookie = resource.headers['set-cookie'];
+        test.create('Cookie information')
+          .get('http://cassandre.local:1337/_session')
+          .addHeader('Cookie', cookie)
+          .expectStatus(200)
+          .expectJSON({name: 'alice'})
+          .after(function() {
+            test.create('Cookie use')
+              .get('http://cassandre.local:1337/text/Wonderland/')
+              .addHeader('Cookie', cookie)
+              .expectStatus(200)
+              .toss();
+          })
+          .toss();
+      })
       .toss();
   })
   .toss();
