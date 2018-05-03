953c5860e14bf45ea198faa339e50b25c7f64ba0
Aurelien Benel <aurelien.benel@utt.fr>
2018-05-03 19:47:03 +0200

    FEATURE: Set a virtual host according to a hostname.
    Breaking change (with v1):
    
    - `proxyHost` syntax is no more a regular expression
    but a string (with or without wildcards).

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const app = require('express')();
+const vhost = require('vhost');
 const configuration = require('./config');
 
 const port = configuration.port;
+
+for (let site of configuration.sites) {
+  app.use(vhost(site.hostProxy, (request, response) => response.send(site.hostProxy)));
+}
 
 app.listen(port, function() {
   console.log(`Test it on http://localhost:${port}`);
