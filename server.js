b49be995ba3eb642d3e20bfc722514c9b1caabf2
Aurelien Benel <aurelien.benel@utt.fr>
2018-05-03 22:30:42 +0200

    FEATURE: Set the vhost as a reverse proxy.
    Options (compatible with v1):
    - `host` (default: `localhost`),
    - `port` (default: `80`),
    - `path` (default: ``),
    - `hideLocationParts` (default: 0).

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 const app = require('express')();
 const vhost = require('vhost');
+const proxy = require('http-proxy-middleware');
 const configuration = require('./config');
 
 const port = configuration.port;
 
+let hideLocationParts = function(site) {
+  return (response) => {
+    if (site.hideLocationParts && response.headers.location) {
+      let locationParts = response.headers.location.split('/');
+      locationParts.splice(3,site.hideLocationParts);
+      response.headers.location = locationParts.join('/');
+    }
+  }
+}
+
 for (let site of configuration.sites) {
-  app.use(vhost(site.hostProxy, (request, response) => response.send(site.hostProxy)));
+  app.use(vhost(site.hostProxy, proxy({
+    target: `http://${site.host||'localhost'}:${site.port||80}`,
+    pathRewrite: {
+      '': site.path||''
+    },
+    onProxyRes: hideLocationParts(site)
+  })));
 }
 
 app.listen(port, function() {
